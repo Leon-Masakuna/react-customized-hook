f6d6f9a9dde5e6dfef4fd97aa6b4165af292ea24
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-05-15 17:47:11 +0200

    feat: react customized hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+//customized hook
+function useIncrement(initial, step) {
+  const [count, setCount] = useState(initial);
+  const increment = () => {
+    setCount(count + step);
+  };
+  return [count, increment];
+}
+
+//App component
 function App() {
+  const [count, increment] = useIncrement(0, 3);
+  const [state, setState] = useState({
+    a: 1,
+    b: 2,
+    c: 3,
+  });
+
+  const changeState = (event) => {
+    event.preventDefault();
+    setState((state) => {
+      return { ...state, count: 10 };
+    });
+  };
+
+  /* const handleClick = (event) => {
+    event.preventDefault();
+    setCount(count + 1);
+  }; */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={increment}>Incrémentez</button>
+        <p>{count}</p>
+        <button onClick={changeState}>
+          Incrémentez{JSON.stringify(state)}
+        </button>
+      </div>
     </div>
   );
 }
